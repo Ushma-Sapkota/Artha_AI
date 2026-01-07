5972337aaa863b7655500bc8ee0e78671a9040d8
Ushma-Sapkota <ushma.sapko@gmail.com>
2026-01-07 17:21:35 +0545

    analyze page

--- myapp/urls.py
+++ myapp/urls.py
@@ -16,4 +16,8 @@ urlpatterns = [
     path('help/', views.help_view, name='help'),
     path('profile/', views.profile, name='profile'),
     path('chatbot/', views.chatbot, name='chatbot'),
+    path('api/weekly_summary/',views.weekly_summary_api,name='weekly_summary_api'),
+    path('api/monthly_category/',views.monthly_category_api,name='monthly_category_api'),
+    path('api/category_trend/', views.category_trend_api, name='category_trend_api'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,13 +5,27 @@ from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from decimal import Decimal
 import random, time
-from django.db.models import Sum
+from django.db.models import Sum, Count
+from django.db.models.functions import ExtractMonth, ExtractYear
 
 from .models import User, Expense, Income, Goal
 from .forms import SignUpForm
 from .models import GoalContribution
 from django.shortcuts import render, redirect, get_object_or_404
 
+from django.http import JsonResponse
+from datetime import date, timedelta, datetime
+import calendar
+from django.db import models
+import numpy as np
+
+# ---------------- Static Pages ----------------
+def budget(request):return render(request, 'myapp/budget.html')
+def review(request): return render(request, 'myapp/review.html')
+def help_view(request): return render(request, 'myapp/help.html')
+def profile(request): return render(request, 'myapp/profile.html')
+def settings_view(request): return render(request, 'myapp/settings.html')
+def chatbot(request): return render(request, 'myapp/chatbot.html')
 
 
 # ---------------- Signup ----------------
@@ -205,11 +219,192 @@ def add_contribution(request, goal_id):
 
     return render(request, "goal_detail.html", {"goal": goal})
 
-# ---------------- Static Pages ----------------
-def analytics(request): return render(request, 'myapp/analytics.html')
-def budget(request):return render(request, 'myapp/budget.html')
-def review(request): return render(request, 'myapp/review.html')
-def help_view(request): return render(request, 'myapp/help.html')
-def profile(request): return render(request, 'myapp/profile.html')
-def settings_view(request): return render(request, 'myapp/settings.html')
-def chatbot(request): return render(request, 'myapp/chatbot.html')
+@login_required(login_url='sigin')
+def analytics(request):
+    user=request.user
+    today=date.today()
+    
+    #display info
+    month_income=Income.objects.filter(user=user, date__year=today.year, date__month=today.month).aggregate(Sum('amount'))['amount__sum'] or 0
+    month_expense=Expense.objects.filter(user=user,date__year=today.year, date__month=today.month).aggregate(Sum('amount'))['amount__sum'] or 0
+
+    active_months=Expense.objects.filter(user=user).dates('date','month').count() or 1
+    total_income=Income.objects.filter(user=user).aggregate(Sum('amount'))['amount__sum'] or 0
+    total_expense=Expense.objects.filter(user=user).aggregate(Sum('amount'))['amount__sum'] or 0
+    avg_income=total_income / active_months
+    avg_expense=total_expense / active_months
+
+    #for monthly history
+    monthly_history=[]
+    recent_months=Expense.objects.filter(user=user).annotate(
+        m=ExtractMonth('date'),
+        y=ExtractYear('date')
+    ).values('m','y').distinct().order_by('-y','-m')[:6]
+
+    for item in recent_months:
+        m_inc=Income.objects.filter(user=user,date__year=item['y'], date__month=item['m']).aggregate(Sum('amount'))['amount__sum'] or 0
+        m_exp=Expense.objects.filter(user=user,date__year=item['y'], date__month=item['m']).aggregate(Sum('amount'))['amount__sum'] or 0
+        m_count=Expense.objects.filter(user=user,date__year=item['y'], date__month=item['m']).count() + \
+                Income.objects.filter(user=user,date__year=item['y'], date__month=item['m']).count()
+        
+        monthly_history.append({
+            'name': calendar.month_name[item['m']],
+            'year': item['y'],
+            'income': m_inc,
+            'expense': m_exp,
+            'count': m_count
+        })
+    
+    category_summary=(
+        Expense.objects.filter(user=user, date__year=today.year, date__month=today.month)
+        .values('category')
+        .annotate(total=Sum('amount'))
+        .order_by('-total')
+    )
+
+    top_categories=[]
+    for cat in category_summary:
+        percentage=(cat['total']/month_expense * 100) if month_expense > 0 else 0
+        top_categories.append({
+            'category': cat['category'],
+            'total': float(cat['total']),
+            'percentage': round(percentage,1)
+        })
+
+    context = {
+        'month_income': month_income,
+        'month_expense': month_expense,
+        'avg_income': avg_income,
+        'avg_expense': avg_expense,
+        'monthly_history': monthly_history,
+        'top_categories': top_categories,
+        }
+    return render(request, 'myapp/analytics.html', context)
+
+
+#Analytics graphs
+@login_required(login_url='signin')
+def weekly_summary_api(request):
+    try:
+        start_str=request.GET.get('start')
+        end_str=request.GET.get('end')
+
+        if start_str and end_str:
+            try:
+                start=datetime.strptime(start_str,"%Y-%m-%d").date()
+                end=datetime.strptime(end_str,"%Y-%m-%d").date()
+
+            except ValueError:
+                end=date.today()
+                start=end-timedelta(days=7)
+
+        else:
+            end=date.today()
+            start=end-timedelta(days=7)
+
+        income = (
+            Income.objects
+            .filter(user=request.user, date__range=[start, end])
+            .values('date')
+            .annotate(total=Sum('amount'), type=models.Value('income'))
+        )
+
+        expense = (
+            Expense.objects
+            .filter(user=request.user, date__range=[start, end])
+            .values('date')
+            .annotate(total=Sum('amount'), type=models.Value('expense'))
+        )
+
+        data = list(income) + list(expense)
+        return JsonResponse({
+                        "start":start,
+                        "end":end,
+                        "data":list(data)  })
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+
+#Pie Chart
+@login_required(login_url='signin')
+def monthly_category_api(request):
+    try:
+        today=date.today()
+        year=int(request.GET.get('year',today.year))
+        month=int(request.GET.get('month',today.month))
+
+
+        data=(
+            Expense.objects
+            .filter(user=request.user,date__year=year,date__month=month)
+            .values('category')
+            .annotate(total=Sum('amount'))
+            .order_by('-total')
+        )
+
+        return JsonResponse({
+            "year":year,
+            "month":month,
+            "data":list(data) })
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+#Line Graph
+def category_trend_api(request):
+    try:
+        today=date.today()
+        year=int(request.GET.get('year',today.year))
+        month=int(request.GET.get('month',today.month))
+        category=request.GET.get('category')
+        days_in_month = calendar.monthrange(year,month)[1]
+    
+        if not category:
+            return JsonResponse({'error': 'Category missing'}, status=400)
+
+
+        qs=(
+            Expense.objects
+            .filter(user=request.user,category=category,date__year=year,date__month=month)
+            .values('date')
+            .annotate(total=Sum('amount'))
+            .order_by('date')
+        )
+
+        days=[
+            date(year,month,d).isoformat()
+            for d in range(1, days_in_month+1)
+        ]
+
+        values=[0]*days_in_month
+
+        for entry in qs:
+            index=days.index(entry['date'].isoformat())
+            values[index]=entry['total']
+
+        days_nums=np.arange(1,days_in_month+1)
+        values_array=np.array(values)
+
+        #min two values needed
+        if len(values_array[values_array>0])>=2:
+            m,b=np.polyfit(days_nums.astype(float),values_array.astype(float), 1)
+            predicted_values=[max(0,float(m*x+b)) for x in days_nums]
+        
+        else:
+            predicted_values=values
+
+        return JsonResponse({
+            "year":year,
+            "month":month,
+            "category":category,
+            "days":days,
+            "values":values,
+            "predicted":predicted_values
+            })
+    
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+
+              
